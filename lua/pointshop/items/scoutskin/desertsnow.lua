ff6589aa86918bb9bd154c460384746fb14c213f
Tomelyr <Tomelyr@googlemail.com>
2015-03-01 16:22:05 +0100

    Untested PS Code change
    since im too lazy

--- lua/pointshop/items/scoutskin/desertsnow.lua
+++ lua/pointshop/items/scoutskin/desertsnow.lua
@@ -3,18 +3,24 @@ ITEM.Name = 'Desert Snow' // Name of the Skin
 ITEM.Price = 200		  // Price of the Skin
 ITEM.Material = 'trails/electric.vmt' // Preview Screenshot? Need to look into the Manual
 
+--[[ Skin Information ]]--
+local SkinShop = {} // needed? Does it break something? test it!
+SkinShop.WeaponClass = "weapon_zm_rifle"
+SkinShop.ViewModel = "skinshop/scout/desertsnow/v_desertsnow"
+SkinShop.WorldModel = "skinshop/scout/desertsnow/w_desertsnow"
+
 --[[ Functions ]]--
 function ITEM:OnEquip(ply, modifications)
-	ply:SetNWString("weapon_zm_rifle_vm", "skinshop/scout/desertsnow/v_desertsnow")
-	ply:SetNWString("weapon_zm_rifle_wm", "skinshop/scout/desertsnow/w_desertsnow")
+	ply:SetNWString(SkinShop.WeaponClass .. "_vm", SkinShop.ViewModel)
+	ply:SetNWString(SkinShop.WeaponClass .. "_wm", SkinShop.WorldModel)
 end
 
 function ITEM:OnHolster(ply)
-	ply:SetNWString("weapon_zm_rifle_vm", "")
-	ply:SetNWString("weapon_zm_rifle_wm", "")
+	ply:SetNWString(SkinShop.WeaponClass .. "_vm", "")
+	ply:SetNWString(SkinShop.WeaponClass .. "_wm", "")
 end
 
 function ITEM:OnSell(ply)
-	ply:SetNWString("weapon_zm_rifle_vm", "")
-	ply:SetNWString("weapon_zm_rifle_wm", "")
+	ply:SetNWString(SkinShop.WeaponClass .. "_vm", "")
+	ply:SetNWString(SkinShop.WeaponClass .. "_wm", "")
 end
